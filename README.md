0f2bfa0acd7531a817196d990037d49da196a4fe
Rowan Mullin <rmullin@academic.rrc.ca>
2025-07-02 10:18:19 -0500

    Updated readme

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Word Game
 
 # Descripton
 This is my CLI recreation of the game 'wordle', I will later add a full UI version in the next iteration.
+
+# Dependancies
+python3
+
+# Installation
+I will be adding a batch script you can run to install the repo, but for now:
+1. Start by creating a new directory/folder or navigate to an old one,
+2. next open a terminal window (example: git bash) in the folder
+3. Type and enter: "git clone https://github.com/rowanmullin2/wordgame-python.git"
+4. Type and enter: "cd wordgame-python"
+5. Type and enter: "python app-cli.py"
--- app-ui.py
+++ app-ui.py
@@ -8,15 +8,32 @@ def createWindow():
 
     # Event listener for button click
     def on_button_click():
-        messagebox.showinfo("Info", "Button clicked!"+" input: "+input_field.get())
+        messagebox.showinfo("Info", "Button clicked!"+" input: "+input_field1.get())
+    
+    # Title display
+    title = ttk.Label(window, text="Word Game", font=('Times New Roman', 30))
+    title.pack(pady=20)
 
     # test button
     button = ttk.Button(window, text="Click Me", command=on_button_click)
     button.pack(pady=20)
 
+    #grid layout
+
     # test input field
-    input_field = ttk.Entry(window, width=10)
-    input_field.pack(pady=20)
+    input_field1 = ttk.Entry(window, font=('Times New Roman', 50), width=1)
+    #input_field1.pack(pady=20, ipady=5)
+    """
+    input_field1.grid(row=0, column=0, padx=10, pady=10)
+    input_field2 = ttk.Entry(window, font=('Times New Roman', 50), width=1)
+    input_field2.pack(pady=20, ipady=5)
+    input_field3 = ttk.Entry(window, font=('Times New Roman', 50), width=1)
+    input_field3.pack(pady=20, ipady=5)
+    input_field4 = ttk.Entry(window, font=('Times New Roman', 50), width=1)
+    input_field4.pack(pady=20, ipady=5)
+    input_field5 = ttk.Entry(window, font=('Times New Roman', 50), width=1)
+    input_field5.pack(pady=20, ipady=5)
+    """
 
     return window
 
